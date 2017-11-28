454d3d786189f4aca36e6650dc1ea168fd91a55f
Carter Hickingbotham <carter@Mac.local>
2017-11-28 14:28:04 -0800

    working through unit testing

--- tests.py
+++ tests.py
@@ -2,14 +2,12 @@ import unittest
 
 from iterable_wrapper import IterableAPI
 
-
+# init class so we can test requests
 API_KEY= "94c3333a8e224b32b93a40788d1927cc"
 ic= IterableAPI(api_key=API_KEY)
 
 class WrapperTestCase(unittest.TestCase):
-    """Tests for `iterable_wrapper.py`."""
-
-    
+    """Tests for `iterable_wrapper.py`."""    
 
 
     def test_users_requests(self):
