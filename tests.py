ec854b5e82fabf960167e143d9c5be3e1d2ddac7
Carter Hickingbotham <carter@Mac.local>
2017-11-28 14:26:07 -0800

    first successful test on generic api request

--- tests.py
+++ tests.py
@@ -2,6 +2,10 @@ import unittest
 
 from iterable_wrapper import IterableAPI
 
+
+API_KEY= "94c3333a8e224b32b93a40788d1927cc"
+ic= IterableAPI(api_key=API_KEY)
+
 class WrapperTestCase(unittest.TestCase):
     """Tests for `iterable_wrapper.py`."""
 
@@ -10,7 +14,7 @@ class WrapperTestCase(unittest.TestCase):
 
     def test_users_requests(self):
         """Is api reqeust behaving correctly?"""
-        self.assertTrue(IterableAPI.api_call(call="/api/users/getByEmail", method="GET", params={"email": "carter@iterable.com"}))
+        self.assertTrue(ic.api_call(call="/api/users/getByEmail", method="GET", params={"email": "carter@iterable.com"}))
 
 if __name__ == '__main__':
     unittest.main()
